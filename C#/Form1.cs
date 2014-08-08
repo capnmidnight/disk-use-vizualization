68ac41f0bef757980565ee2e0fd263c9ecf55fc5
Sean T. McBeth <sean.mcbeth@gmail.com>
2014-08-08 08:34:28 -0400

    updates to work in general

--- C#/Form1.cs
+++ C#/Form1.cs
@@ -36,38 +36,44 @@ namespace DiskUseViz
             if (progressBar1.InvokeRequired)
                 progressBar1.Invoke(new Action<int>(Progress), i);
             else
-                progressBar1.Value = i;
+                progressBar1.Value = Math.Max(progressBar1.Minimum, Math.Min(progressBar1.Maximum, i));
         }
         Dictionary<Color, string> pickKeys = new Dictionary<Color, string>();
         private Image RenderImage(string root, Size size)
         {
             var dirSizes = new Dictionary<string, double>();
             var dirs = new List<string>();
-            var files = new List<string>();
             dirs.Add(root);
             int p = 0;
+            int len = 1;
             while (dirs.Count > 0)
             {
-                Progress(p / 10);
-                p = (p + 1) % 1000;
+                Progress(p++ * 100 / len);
                 var dir = dirs[0];
                 dirs.RemoveAt(0);
                 var di = new DirectoryInfo(dir);
-                if (di.Name != "hive76" && di.Exists)
+                if (di.Exists)
                 {
-                    dirs.AddRange(Directory.GetDirectories(dir));
-                    dirSizes.Add(dir, 0);
-                    foreach (var f in Directory.GetFiles(dir))
+                    try
                     {
-                        var fi = new FileInfo(f);
-                        dirSizes.Add(f, fi.Length);
-                        var next = di;
-                        while (dirSizes.ContainsKey(next.FullName))
+                        var subDirs = Directory.GetDirectories(dir);
+                        len += subDirs.Length;
+                        dirs.AddRange(subDirs);
+                        dirSizes.Add(di.FullName, 0);
+                        var dirFiles = Directory.GetFiles(dir);
+                        foreach (var f in dirFiles)
                         {
-                            dirSizes[next.FullName] += fi.Length;
-                            next = next.Parent;
+                            var fi = new FileInfo(f);
+                            dirSizes.Add(fi.FullName, fi.Length);
+                            var next = di;
+                            while (next != null && dirSizes.ContainsKey(next.FullName))
+                            {
+                                dirSizes[next.FullName] += fi.Length;
+                                next = next.Parent;
+                            }
                         }
                     }
+                    catch { }
                 }
                 Application.DoEvents();
             }
@@ -75,39 +81,50 @@ namespace DiskUseViz
             dirs.Add(root);
             var rects = new Dictionary<string, Rectangle>();
             rects.Add(root, new Rectangle(0, 0, size.Width, size.Height));
+            len = dirs.Count;
             while (dirs.Count > 0)
             {
-                Progress(p / 10);
-                p = (p + 1) % 1000;
+                Progress(p++ * 100 / len);
                 var dir = dirs[0];
                 dirs.RemoveAt(0);
                 if (Directory.Exists(dir))
                 {
-                    var subDirs = Directory.GetDirectories(dir)
-                        .Where(d=>!d.EndsWith("hive76"))
-                        .Union(Directory.GetFiles(dir))
-                        .ToArray();
-                    Array.Sort(subDirs, (di1, di2) => (int)(dirSizes[di2] - dirSizes[di1]));
-                    dirs.AddRange(subDirs);
-                    var flip = false;
-                    if (rects.ContainsKey(dir))
+                    try
                     {
-                        var space = rects[dir];
-                        foreach (var subDir in subDirs)
+                        var subDirs = Directory.GetDirectories(dir)
+                            .Select(d => new DirectoryInfo(d).FullName)
+                            .Union(Directory.GetFiles(dir)
+                                .Select(f => new FileInfo(f).FullName))
+                            .ToArray();
+                        Array.Sort(subDirs, (di1, di2) =>
+                            dirSizes.ContainsKey(di1) && dirSizes.ContainsKey(di2)
+                            ? (int)(dirSizes[di2] - dirSizes[di1])
+                            : -1);
+                        dirs.AddRange(subDirs);
+                        var flip = false;
+                        if (rects.ContainsKey(dir))
                         {
-                            var percentage = dirSizes[subDir] / dirSizes[dir];
-                            int width = (int)(space.Width * (flip ? 1 : percentage));
-                            int height = (int)(space.Height * (flip ? percentage : 1));
-                            var subRect = new Rectangle(space.X, space.Y, width, height);
-                            rects.Add(subDir, subRect);
-                            space = new Rectangle(
-                                space.X + (flip ? 0 : width),
-                                space.Y + (flip ? height : 0),
-                                space.Width - (flip ? 0 : width),
-                                space.Height - (flip ? height : 0));
-                            flip = !flip;
+                            var space = rects[dir];
+                            foreach (var subDir in subDirs)
+                            {
+                                if (dirSizes.ContainsKey(subDir) && dirSizes.ContainsKey(dir))
+                                {
+                                    var percentage = dirSizes[subDir] / dirSizes[dir];
+                                    int width = (int)(space.Width * (flip ? 1 : percentage));
+                                    int height = (int)(space.Height * (flip ? percentage : 1));
+                                    var subRect = new Rectangle(space.X, space.Y, width, height);
+                                    rects.Add(subDir, subRect);
+                                    space = new Rectangle(
+                                        space.X + (flip ? 0 : width),
+                                        space.Y + (flip ? height : 0),
+                                        space.Width - (flip ? 0 : width),
+                                        space.Height - (flip ? height : 0));
+                                    flip = !flip;
+                                }
+                            }
                         }
                     }
+                    catch { }
                 }
                 Application.DoEvents();
             }
@@ -123,7 +140,7 @@ namespace DiskUseViz
                     if (rect.Width > 0 && rect.Height > 0)
                     {
                         var color = Color.FromArgb((int)(0xff000000 | (uint)c));
-                        c+=0x007001;
+                        c += 0x007001;
                         if (!pickKeys.ContainsKey(color))
                             pickKeys.Add(color, rs[i]);
                         g.FillRectangle(new SolidBrush(color), rect);
